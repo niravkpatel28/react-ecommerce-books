7fdd1daed73755f1810645f344a9fa5beb81ec07
Nirav Patel <niravkpatel28@gmail.com>
2020-05-23 14:57:09 +0530

    Updates

---
+++ precache-manifest.7e6fe7dd4a496a2f4107abd21aabb603.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d54b9448a85113059e3b7ce2182f992",
+    "url": "/react-ecommerce-books/index.html"
+  },
+  {
+    "revision": "c9c990822a8cd0d00eec",
+    "url": "/react-ecommerce-books/static/css/main.77ce7aef.chunk.css"
+  },
+  {
+    "revision": "642ed1a6262c2502eac0",
+    "url": "/react-ecommerce-books/static/js/2.e38a481b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-ecommerce-books/static/js/2.e38a481b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9c990822a8cd0d00eec",
+    "url": "/react-ecommerce-books/static/js/main.ad52803e.chunk.js"
+  },
+  {
+    "revision": "eca55116e25f43b2a3d2",
+    "url": "/react-ecommerce-books/static/js/runtime-main.bf2066ff.js"
+  }
+]);
